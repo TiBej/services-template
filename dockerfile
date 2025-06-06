866d99cd6366aadeb17de2655af52d1dbe011fa0
Tim Elsener <tim.elsener@ecologic.ch>
2025-06-06 22:52:50 +0200

    renaming service

--- dockerfile
+++ dockerfile
@@ -1,14 +1,14 @@
-# Use the official Python image.
 FROM python:3.13-alpine3.21
+ARG SERVICE_NAME
 
-# Set the working directory in the container.
 WORKDIR /app
 
-COPY example-service ./example-service
 COPY common-lib ./common-lib
-WORKDIR /app/example-service
+COPY ${SERVICE_NAME} ./${SERVICE_NAME}
+WORKDIR /app/${SERVICE_NAME}
 
 RUN pip install poetry
 RUN poetry config virtualenvs.create false
 RUN poetry install
-RUN poetry run start
+
+CMD ["poetry", "run", "start"]
---
+++ first-service/first_service/main.py
@@ -0,0 +1,24 @@
+from common_lib.loggingfw import CustomLogFW
+
+from flask import Flask, request, session, jsonify
+import logging
+
+logFW = CustomLogFW(service_name='user_service', instance_id='2')
+handler = logFW.setup_logging()
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(handler)
+
+app = Flask(__name__)
+
+@app.route('/trigger_log', methods=['GET'])
+def bug():
+    logging.info("bug is incoming")
+    logging.critical("critical message")
+    logging.error("Triggering bug...")
+    return "Log triggered", 200
+
+def main():
+    app.run(host="0.0.0.0", port=5001)
+
+if __name__ == '__main__':
+    main();
