a409176f2e4d5456eec7ba7ee1bca95f7bbdebb3
Tim Elsener <tim.elsener@ecologic.ch>
2025-06-11 12:28:36 +0200

    rename first-service to api-service

--- api-service/main.py
+++ api-service/main.py
@@ -1,15 +1,15 @@
 import logging
 
-from common.utilities.logging_fw import LoggingFW
 from fastapi import FastAPI
-
 from routers import users
 
-logFW = LoggingFW(service_name="first-service")
+from common.utilities.logging_fw import LoggingFW
+
+logFW = LoggingFW(service_name="api-service")
 handler = logFW.setup_logging()
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(handler)
 
-app = FastAPI(title="first-service")
+app = FastAPI(title="api-service")
 
 app.include_router(users.router)
